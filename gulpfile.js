30154a7edbe61d4362b0076aaf6a1e8c555b09dc
DayDay <daydaybigking@sina.com>
2020-10-14 14:01:09 +0800

    接着修改

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const { src, dest, series, watch } = require("gulp");
+const del = require("delete");
+const sass = require("gulp-sass");
+
+sass.compiler = require("dart-sass");
+
+function clean(cb) {
+  return del(["dist"], cb);
+}
+
+function sassTask() {
+  return src("src/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("dist"));
+}
+
+const defaultTask = series(clean, sassTask);
+
+watch(["src/*.scss"], defaultTask);
+
+exports.default = defaultTask;
