adfb4b37332d6481f6351ed2a432ea6a3e589165
DayDay <daydaybigking@sina.com>
2020-10-15 16:42:24 +0800

    基本完成加法和等于的逻辑

--- dist/index.js
+++ dist/index.js
@@ -15,12 +15,15 @@ var Calculator = /*#__PURE__*/function () {
 
     this.numbers = document.querySelectorAll(".btn-number"); // 符号按钮
 
-    this.sign = document.querySelectorAll('.btn-sign'); // 运算符按钮
+    this.sign = document.querySelectorAll(".btn-sign"); // 运算符按钮
 
-    this.operator = document.querySelectorAll('.btn-operator'); // 
+    this.operator = document.querySelectorAll(".btn-operator"); //
     // 屏幕显示数字最长长度
 
-    this.limit = 9;
+    this.limit = 9; // 点击运算符后暂存的值
+
+    this.cache = [];
+    this.operatorJustClick = false;
   }
   /**
    * 初始化组件
@@ -60,14 +63,23 @@ var Calculator = /*#__PURE__*/function () {
       var text = this.screen.innerText;
 
       if (text.length < this.limit) {
-        if (text.toString() === '0') {
+        if (text.toString() === "0") {
           // 如果此时为0, 除小数点外是追加, 其他都是覆盖
-          if (val === '.' && !text.includes(val)) {
+          if (val === "." && !text.includes(val)) {
             this.screen.innerText += val;
           } else {
             this.setScreen(val);
           }
-        } else if (!(val === '.' && text.includes(val))) {
+        } else if (this.operatorJustClick) {
+          // 如果是刚点击运算符, 下一次输入就需要替换屏幕中的内容
+          if (val === '.') {
+            this.setScreen('0.');
+          } else {
+            this.setScreen(val);
+          }
+
+          this.operatorJustClick = false;
+        } else if (!(val === "." && text.includes(val))) {
           // 必须保证数字中只有一个小数点
           this.screen.innerText += val;
         }
@@ -121,7 +133,7 @@ var Calculator = /*#__PURE__*/function () {
 
       this.sign.forEach(function (item) {
         switch (item.innerText) {
-          case 'C':
+          case "C":
             item.onclick = function () {
               return _this2.clearScreen();
             };
@@ -136,7 +148,64 @@ var Calculator = /*#__PURE__*/function () {
 
   }, {
     key: "bindOperatorHandles",
-    value: function bindOperatorHandles() {}
+    value: function bindOperatorHandles() {
+      var _this3 = this;
+
+      this.operator.forEach(function (item) {
+        var text = item.innerText;
+
+        item.onclick = function () {
+          _this3.clearOperatorState();
+
+          if (text !== '=') {
+            item.classList.add("active");
+
+            _this3.cache.push(_this3.screen.innerText);
+
+            _this3.cache.push(text);
+
+            _this3.operatorJustClick = true;
+          } else {
+            _this3.cache.push(_this3.screen.innerText);
+
+            _this3.setScreen(_this3.calculate());
+
+            _this3.operatorJustClick = true;
+          }
+        };
+      });
+    }
+    /**
+     * 所有运算按钮恢复默认状态
+     */
+
+  }, {
+    key: "clearOperatorState",
+    value: function clearOperatorState() {
+      this.operator.forEach(function (item) {
+        item.classList.remove('active');
+      });
+    }
+  }, {
+    key: "calculate",
+    value: function calculate() {
+      var _this4 = this;
+
+      var total;
+      this.cache.forEach(function (item, index) {
+        if (index === 0) {
+          total = Number(item);
+        } else {
+          switch (item) {
+            case "+":
+              total += Number(_this4.cache[index + 1]);
+              break;
+          }
+        }
+      });
+      this.cache = [];
+      return total;
+    }
   }]);
 
   return Calculator;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ class Calculator {
     // 数字按钮
     this.numbers = document.querySelectorAll(`.btn-number`);
     // 符号按钮
-    this.sign = document.querySelectorAll('.btn-sign');
+    this.sign = document.querySelectorAll(".btn-sign");
     // 运算符按钮
-    this.operator = document.querySelectorAll('.btn-operator');
-    // 
+    this.operator = document.querySelectorAll(".btn-operator");
+    //
     // 屏幕显示数字最长长度
     this.limit = 9;
+
+    // 点击运算符后暂存的值
+    this.cache = [];
+    this.operatorJustClick = false;
   }
   /**
    * 初始化组件
@@ -39,14 +43,22 @@ class Calculator {
   appendScreen(val) {
     const text = this.screen.innerText;
     if (text.length < this.limit) {
-      if (text.toString() === '0') {
+      if (text.toString() === "0") {
         // 如果此时为0, 除小数点外是追加, 其他都是覆盖
-        if (val === '.' && !text.includes(val)) {
+        if (val === "." && !text.includes(val)) {
           this.screen.innerText += val;
         } else {
           this.setScreen(val);
         }
-      } else if (!(val === '.' && text.includes(val))) {
+      } else if (this.operatorJustClick) {
+        // 如果是刚点击运算符, 下一次输入就需要替换屏幕中的内容
+        if (val === '.') {
+          this.setScreen('0.');
+        } else {
+          this.setScreen(val);
+        }
+        this.operatorJustClick = false;
+      } else if (!(val === "." && text.includes(val))) {
         // 必须保证数字中只有一个小数点
         this.screen.innerText += val;
       }
@@ -81,17 +93,56 @@ class Calculator {
   bindSignHandles() {
     this.sign.forEach((item) => {
       switch (item.innerText) {
-        case 'C':
+        case "C":
           item.onclick = () => this.clearScreen();
           break;
       }
-    })
+    });
   }
   /**
    * 绑定运算符按钮点击事件
    */
   bindOperatorHandles() {
-    
+    this.operator.forEach((item) => {
+      const text = item.innerText;
+      item.onclick = () => {
+        this.clearOperatorState();
+        if (text !== '=') {
+          item.classList.add("active");
+          this.cache.push(this.screen.innerText);
+          this.cache.push(text);
+          this.operatorJustClick = true;
+        } else {
+          this.cache.push(this.screen.innerText);
+          this.setScreen(this.calculate());
+          this.operatorJustClick = true;
+        }
+      }
+    });
+  }
+  /**
+   * 所有运算按钮恢复默认状态
+   */
+  clearOperatorState() {
+    this.operator.forEach((item) => {
+      item.classList.remove('active');
+    })
+  }
+  calculate() {
+    let total;
+    this.cache.forEach((item, index) => {
+      if (index === 0) {
+        total = Number(item);
+      } else {
+        switch (item) {
+          case "+":
+            total += Number(this.cache[index + 1]);
+            break;
+        }
+      }
+    })
+    this.cache = [];
+    return total;
   }
 }
 
