86927bc5272fced6df49847791372e7eee32d8a2
DayDay <daydaybigking@sina.com>
2020-10-15 15:35:24 +0800

    完成一版本, 实现最佳数字和归零

--- dist/index.js
+++ dist/index.js
@@ -10,24 +10,70 @@ var Calculator = /*#__PURE__*/function () {
   function Calculator() {
     _classCallCheck(this, Calculator);
 
-    this.screen = null;
-    this.numbers = null;
+    // 屏幕
+    this.screen = document.querySelector(".screen"); // 数字按钮
+
+    this.numbers = document.querySelectorAll(".btn-number"); // 归零按钮
+
+    this.signZero = document.querySelector('.btn-sign-zero'); // 屏幕显示数字最长长度
+
+    this.limit = 9;
   }
   /**
-   * 传入屏幕和按钮 css class
+   * 初始化组件
    * @param {{screen:string, numbers: string}} data
    */
 
 
   _createClass(Calculator, [{
     key: "init",
-    value: function init(_ref) {
-      var screen = _ref.screen,
-          numbers = _ref.numbers;
-      this.screen = document.querySelector(screen);
-      this.numbers = document.querySelectorAll(number); // 绑定点击事件
+    value: function init() {
+      // 初始化屏幕
+      this.clearScreen(); // 绑定点击事件
 
       this.bindHandles();
+    }
+    /**
+     * 设置屏幕显示的值, 并返回当前屏幕上显示的值
+     * @param {string} val
+     * @return {string}
+     */
+
+  }, {
+    key: "setScreen",
+    value: function setScreen(val) {
+      this.screen.innerText = val;
+      return val;
+    }
+    /**
+     * 追加屏幕值, 并返回追加后屏幕显示的值
+     * @param {string} val
+     * @return {string}
+     */
+
+  }, {
+    key: "appendScreen",
+    value: function appendScreen(val) {
+      var text = this.screen.innerText;
+
+      if (text.length < this.limit) {
+        if (text.toString() === "0") {
+          this.setScreen(val);
+        } else {
+          this.screen.innerText += val;
+        }
+      }
+
+      return this.screen.innerText;
+    }
+    /**
+     * 屏幕归零
+     */
+
+  }, {
+    key: "clearScreen",
+    value: function clearScreen() {
+      this.setScreen(0);
     }
     /**
      * 绑定点击事件
@@ -37,6 +83,7 @@ var Calculator = /*#__PURE__*/function () {
     key: "bindHandles",
     value: function bindHandles() {
       this.bindNumberHandles();
+      this.bindSignHandles();
     }
     /**
      * 绑定数字按钮点击事件
@@ -45,14 +92,30 @@ var Calculator = /*#__PURE__*/function () {
   }, {
     key: "bindNumberHandles",
     value: function bindNumberHandles() {
-      this.number;
+      var _this = this;
+
+      this.numbers.forEach(function (item) {
+        item.onclick = function () {
+          return _this.appendScreen(item.innerText);
+        };
+      });
+    }
+    /**
+     * 绑定 sign 按钮
+     */
+
+  }, {
+    key: "bindSignHandles",
+    value: function bindSignHandles() {
+      var _this2 = this;
+
+      this.signZero.onclick = function () {
+        return _this2.clearScreen();
+      };
     }
   }]);
 
   return Calculator;
 }();
 
-new Calculator().init({
-  screen: '.screen',
-  numbers: ".btn-number"
-});
+new Calculator().init();
--- src/index.js
+++ src/index.js
@@ -1,33 +1,76 @@
 class Calculator {
   constructor() {
-    this.screen = null;
-    this.numbers = null;
+    // 屏幕
+    this.screen = document.querySelector(`.screen`);
+    // 数字按钮
+    this.numbers = document.querySelectorAll(`.btn-number`);
+    // 归零按钮
+    this.signZero = document.querySelector('.btn-sign-zero');
+    // 屏幕显示数字最长长度
+    this.limit = 9;
   }
   /**
-   * 传入屏幕和按钮 css class
+   * 初始化组件
    * @param {{screen:string, numbers: string}} data
    */
-  init({ screen, numbers }) {
-    this.screen = document.querySelector(screen);
-    this.numbers = document.querySelectorAll(number);
+  init() {
+    // 初始化屏幕
+    this.clearScreen();
     // 绑定点击事件
     this.bindHandles();
+  }
+  /**
+   * 设置屏幕显示的值, 并返回当前屏幕上显示的值
+   * @param {string} val
+   * @return {string}
+   */
+  setScreen(val) {
+    this.screen.innerText = val;
+    return val;
+  }
+  /**
+   * 追加屏幕值, 并返回追加后屏幕显示的值
+   * @param {string} val
+   * @return {string}
+   */
+  appendScreen(val) {
+    const text = this.screen.innerText;
+    if (text.length < this.limit) {
+      if (text.toString() === "0") {
+        this.setScreen(val);
+      } else {
+        this.screen.innerText += val;
+      }
+    }
+    return this.screen.innerText;
+  }
+  /**
+   * 屏幕归零
+   */
+  clearScreen() {
+    this.setScreen(0);
   }
   /**
    * 绑定点击事件
    */
   bindHandles() {
     this.bindNumberHandles();
+    this.bindSignHandles();
   }
   /**
    * 绑定数字按钮点击事件
    */
   bindNumberHandles() {
-    this.number
+    this.numbers.forEach((item) => {
+      item.onclick = () => this.appendScreen(item.innerText);
+    });
+  }
+  /**
+   * 绑定 sign 按钮
+   */
+  bindSignHandles() {
+    this.signZero.onclick = () => this.clearScreen();
   }
 }
 
-new Calculator().init({
-  screen: '.screen',
-  numbers: ".btn-number"
-});
+new Calculator().init();
